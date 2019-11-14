6ded2988dbc87ef838fd4ed54b3c56f104eda5c8
LOUIS François <francoois.louis@hotmail.fr>
2019-11-14 01:33:00 +0100

    fix token issues

--- app/src/main/java/fr/android/watermelon/LoginActivity.java
+++ app/src/main/java/fr/android/watermelon/LoginActivity.java
@@ -2,7 +2,9 @@ package fr.android.watermelon;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -86,6 +88,7 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this, "Something went wrong...Please try again!", Toast.LENGTH_SHORT).show();
                 else {
                     //saveAccessToken(response.body().toString());
+                    setDefaults("access_token",response.body().toString(),LoginActivity.this);
                     reloadMain = true;
                     finish();
                 }
